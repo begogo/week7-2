3f654efc6cde682559d94a3a9acf9cfd1b0c3d63
gongbu <asd@asd.com>
2021-05-31 04:32:06 +0900

    개발환경 설정 (상세있음)
    spring initializr (https://start.spring.io/)
    - Gradle, Java 11, Spring Boot 2.3.11, Jar
    - Dependencies: Spring Web, Thymeleaf
    - 인텔리제이 설정 - build - Gradle에서 [Build and run using]과 [Run tests using] 항목을 인텔리제이로 설정

---
+++ src/main/java/com/example/week72/Week72Application.java
@@ -0,0 +1,13 @@
+package com.example.week72;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week72Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week72Application.class, args);
+	}
+
+}
